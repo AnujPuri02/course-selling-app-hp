a8855e4fc6ba3c05ea8f12d1c9771bd80ae4ec57
anuj <anujpuri635@gmail.com>
2025-05-14 13:04:35 +0530

    updated config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const JWT_ADMIN_SECRET= process.env.JWT_ADMIN_SECRET;
 const JWT_USER_SECRET= process.env.JWT_USER_SECRET;
 const MONGO_URL = process.env.MONGO_URL;
+require("dotenv").config()
 
 module.exports={
     JWT_ADMIN_SECRET,
